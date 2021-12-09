95da24a598495bf8b9daf6dff525011fc828911b
BenPrevel <403rutabaga@gmail.com>
2021-12-09 14:41:39 +0100

    Quest Dates Java done

---
+++ TestDate.java
@@ -0,0 +1,34 @@
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Date;
+import java.util.Scanner; 
+
+class TestDate {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please fill a day (1-31) :");
+        int day = sc.nextInt();
+        System.out.println("Please fill a month (1-12) :");
+        int month = sc.nextInt();
+        System.out.println("Please fill a year :");
+        int year = sc.nextInt();
+        
+        
+        sc.close();
+
+        // TODO : your code after this line
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        Calendar userCalendar = new GregorianCalendar(year, month-1, day);
+        Date userDate = userCalendar.getTime();
+        int dayOfTheWeek = userCalendar.get(Calendar.DAY_OF_WEEK);
+        System.out.println("The date is : " + sdf.format(userDate));
+        System.out.println("The day of the week is : " + dayOfTheWeek );
+
+        
+    }
+}
